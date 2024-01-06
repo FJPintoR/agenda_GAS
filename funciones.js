e8ff160a486a1b986950c4f5a16e71bd61c45dd7
FJPINTO <freddy.jpr88@gmail.com>
2024-01-06 00:43:27 -0400

    video9-curso-GAS-Avanzado

--- funciones.js
+++ funciones.js
@@ -1,8 +1,14 @@
-function doGet (){
+function doGet(){
     return HtmlService.createTemplateFromFile('web').evaluate()
     .setTitle('Agenda de Google Apps Scritp');
 }
 
 function obtenerDatosHtml(nombre){
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+}
+
+function obtenerContactos(){
+    let hoja = SpreadsheetApp.openById('1UQ-KmwczgYkh4yzbsnFCN3jFrjRQ6gTPiIU88H24bEg').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
 }
